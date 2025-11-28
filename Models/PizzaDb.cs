813880b1dda6807bd44020640e7034f3ad43cd57
TsiOn <tshimelis23@gmail.com>
2025-11-28 20:18:20 +0300

    first commit

---
+++ Models/PizzaDb.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace PizzaStore.Models
+{
+    public class PizzaDb : DbContext
+    {
+        public PizzaDb(DbContextOptions options) : base(options) { }
+
+        public DbSet<Pizza> Pizzas { get; set; } = null!;
+    }
+}
